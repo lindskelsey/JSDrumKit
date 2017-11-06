cbf183eee38ba0965c2dd5c557bc07b01d54c8de
lindskelsey <lindsaykelsey@gmail.com>
2017-11-06 17:02:40 -0600

    add function to remove transitions, fixed function parameters

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 
 
-function playSoundTransition (event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key ="${event.keyCode}"]`)
+function playSoundTransition(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key ="${e.keyCode}"]`)
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
 }
 
-
+function endTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned', endTransition));
+keys.forEach(key => key.addEventListener('transitionend', endTransition));
 window.addEventListener('keydown', playSoundTransition);
